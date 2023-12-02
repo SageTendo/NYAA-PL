c8437fed0cd55eb6d3910bc1d05787b8e876e658
Nyasha <nz.zishiri@gmail.com>
2023-12-02 23:45:40 +0200

    REPL class impl

---
+++ src/repl/Repl.py
@@ -0,0 +1,35 @@
+from src.utils.ErrorHandler import SUCCESS, ENDC
+
+
+class Repl:
+    def __init__(self, parser, interpreter):
+        self.parser = parser
+        self.interpreter = interpreter
+
+    def run(self, dflags=None):
+        print(f"{SUCCESS}Ohayo!!!\n"
+              f"Welcome to the Nyaa REPL! (◕‿◕✿) Type 'yamete()' to exit. (｡♥‿♥｡)\n{ENDC}")
+
+        while True:
+            line = self.handle_input()
+            AST = self.parser.parse_source(repl_input=line, dflags=dflags)
+            print(AST)
+
+            print(self.interpreter.interpret(AST))
+
+    @staticmethod
+    def handle_input():
+        line = str(input("REPL> "))
+
+        # Exit
+        if line == "yamete()":
+            exit(0)
+
+        # Handle multiline input
+        if line.endswith('{'):
+            next_line = str(input())
+            while not next_line.endswith('}'):
+                line += next_line
+                next_line = str(input())
+            line += next_line
+        return line
